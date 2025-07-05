70576b02817642376ffba3fa7b236efc61157c36
Andrew van Campen <andrewvancampen@gmail.com>
2025-07-05 17:10:47 -0400

    Changed order of functions

--- Arduino-strain-gauges/Arduino-strain-gauges.ino
+++ Arduino-strain-gauges/Arduino-strain-gauges.ino
@@ -4,6 +4,56 @@
 #define minADC A0
 #define maxADC A1
 
+// Read configuration registers
+unsigned long readConfig() {
+    unsigned long output;
+    digitalWrite(ADC, LOW);
+    SPI.transfer(0x23);  // Instruction to read 4 bytes starting at register 0
+    for (int i = 1; i <= 4; i++) {
+        output = (output << 8) + SPI.transfer(0);
+    }
+    digitalWrite(ADC, HIGH);
+    return output;
+}
+
+unsigned long readData(unsigned char ADC) {
+    digitalWrite(ADC, LOW);
+    SPI.transfer(0x10);  // Instruction to read data
+    unsigned long data = 0;
+    for (int i = 1; i <= 3; i++) {  // Get data bytes
+        data = (data << 8) + SPI.transfer(0);
+    }
+    digitalWrite(ADC, HIGH);
+    return data;
+}
+
+// Convert an N-bit two's complement number to a long integer
+long twosComplement(unsigned long num, unsigned char N) {
+    long output;
+    if (num >> (N - 1)) {
+        output = -1 * (~((num - 1) << (33 - N)) >> (33 - N));
+        if (output == 0) {
+            output = -1;
+            for (int i = 1; i <= N - 1; i++) {
+                output = output * 2;
+            }
+        }
+    } else {
+        output = num;
+    }
+    return output;
+}
+
+// Convert output from ADC into a readable value
+int convertOutput(unsigned long data) {
+    //long minval = -1;
+    //for (int i = 1; i <= 23; i++) minval = minval * 2;
+    long maxval = 1;
+    for (int i = 1; i <= 23; i++) maxval = maxval * 2;
+    maxval = maxval - 1;
+    return (1000 * (twosComplement(data, 24) )) / (maxval);
+}
+
 void setup() {
     Serial.begin(9600);
     
@@ -17,9 +67,9 @@ void setup() {
     
     // Reset ADCs
     for (int i = minADC; i <= maxADC; i++) {
-        digitalWrite(ADC, LOW);
+        digitalWrite(i, LOW);
         SPI.transfer(0x06);  // Instruction to reset ADC
-        digitalWrite(ADC, HIGH);
+        digitalWrite(i, HIGH);
     }
     delay(250);
         
@@ -43,55 +93,9 @@ void setup() {
 }
 
 void loop() {
-    
+    Serial.print(convertOutput(readData(A0)));
+    Serial.print(",");
+    Serial.println(convertOutput(readData(A1)));
+    delay(1000);
 }
 
-unsigned long readData() {
-    digitalWrite(ADC, LOW);
-    SPI.transfer(0x10);  // Instruction to read data
-    unsigned long data = 0;
-    for (int i = 1; i <= 3; i++) {  // Get data bytes
-        data = (data << 8) + SPI.transfer(0);
-    }
-    digitalWrite(ADC, HIGH);
-    return data;
-}
-
-// Convert output from ADC into a readable value
-int convertOutput(unsigned long data) {
-    //long minval = -1;
-    //for (int i = 1; i <= 23; i++) minval = minval * 2;
-    long maxval = 1;
-    for (int i = 1; i <= 23; i++) maxval = maxval * 2;
-    maxval = maxval - 1;
-    return (1000 * (twosComplement(data, 24) )) / (maxval);
-}
-
-// Convert an N-bit two's complement number to a long integer
-long twosComplement(unsigned long num, unsigned char N) {
-    long output;
-    if (num >> (N - 1)) {
-        output = -1 * (~((num - 1) << (33 - N)) >> (33 - N));
-        if (output == 0) {
-            output = -1;
-            for (int i = 1; i <= N - 1; i++) {
-                output = output * 2;
-            }
-        }
-    } else {
-        output = num;
-    }
-    return output;
-}
-
-// Read configuration registers
-unsigned long readConfig() {
-    unsigned long output;
-    digitalWrite(ADC, LOW);
-    SPI.transfer(0x23);  // Instruction to read 4 bytes starting at register 0
-    for (int i = 1; i <= 4; i++) {
-        output = (output << 8) + SPI.transfer(0);
-    }
-    digitalWrite(ADC, HIGH);
-    return output;
-}
