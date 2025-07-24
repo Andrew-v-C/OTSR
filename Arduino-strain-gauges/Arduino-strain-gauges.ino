a9b3c124fa733b27119d0a63dfb662b6ffd6e14d
Andrew-v-C <andrewvancampen@gmail.com>
2025-07-24 11:22:07 -0400

    Some changes

--- Arduino-strain-gauges/Arduino-strain-gauges.ino
+++ Arduino-strain-gauges/Arduino-strain-gauges.ino
@@ -2,27 +2,31 @@
 #include <SD.h>
 
 // Chip select (CS) pins
-#define minADC A0
-#define maxADC A1
+#define minADC 2
+#define maxADC 2
 #define SDpin 9
 
-int gaugeData[4];
+// Create data array
+const int dataLen = (1 + maxADC - minADC) * 2 + 1;  // Length = number of gauges + 1 for time stamp
+long gaugeData[dataLen];
 
 void setup() {
     Serial.begin(9600);
 
-    // Initialize SPI
-    SPI.begin();
-    SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE1));
-    for (int i = minADC; i <= maxADC; i++) {  // Initialize ADC pins
+    // Initialize pins
+    pinMode(SDpin, OUTPUT);
+    digitalWrite(SDpin, HIGH);
+    for (int i = minADC; i <= maxADC; i++) {
         pinMode(i, OUTPUT);
         digitalWrite(i, HIGH);
     }
 
-    // Initialize SD
-    pinMode(SDpin, OUTPUT);
+    // Initialize SD and SPI
+    SPI.begin();
+    digitalWrite(SDpin, LOW);
+    SD.begin();
     digitalWrite(SDpin, HIGH);
-    SD.begin(SDpin);
+    SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE1));
 
     // Reset ADCs
     for (int i = minADC; i <= maxADC; i++) {
@@ -40,7 +44,6 @@ void setup() {
         SPI.transfer(0xC4);  // Settings for register 1
         SPI.transfer(0xC0);  // Settings for register 2
         digitalWrite(i, HIGH);
-        //Serial.println(readConfig(i), HEX);  // Print register settings
     }
 
     // Start conversions
@@ -49,36 +52,35 @@ void setup() {
         SPI.transfer(0x08);  // Instruction to start conversions
         digitalWrite(i, HIGH);
     }
+
+    // Test
+    gaugeData[0] = millis();
 }
 
 void loop() {
 }
 
+// Write integer data to SD card
+void writeToSD(long data[], String filename) {
+    digitalWrite(SDpin, LOW);
+    File dataFile = SD.open(filename, FILE_WRITE);
+    for (int i = 0; i <= dataLen - 1; i++) {
+        dataFile.print(data[i]);
+        if (i < dataLen - 1) dataFile.print(", ");
+    }
+    dataFile.println();
+    dataFile.close();
+    digitalWrite(SDpin, HIGH);
+}
+
 // Convert output from ADC into a readable value
 int convertOutput(unsigned long data) {
-    //long minval = -1;
-    //for (int i = 1; i <= 23; i++) minval = minval * 2;
+    // long minval = -1;
+    // for (int i = 1; i <= 23; i++) minval = minval * 2;
     long maxval = 1;
     for (int i = 1; i <= 23; i++) maxval = maxval * 2;
     maxval = maxval - 1;
     return (1000 * (twosComplement(data, 24) )) / (maxval);
-}
-
-// Convert an N-bit two's complement number to a long integer
-long twosComplement(unsigned long num, unsigned char N) {
-    long output;
-    if (num >> (N - 1)) {
-        output = -1 * (~((num - 1) << (33 - N)) >> (33 - N));
-        if (output == 0) {
-            output = -1;
-            for (int i = 1; i <= N - 1; i++) {
-                output = output * 2;
-            }
-        }
-    } else {
-        output = num;
-    }
-    return output;
 }
 
 // Read raw data from ADC
@@ -102,5 +104,22 @@ unsigned long readConfig(int chipSelect) {
         output = (output << 8) + SPI.transfer(0);
     }
     digitalWrite(chipSelect, HIGH);
+    return output;
+}
+
+// Convert an N-bit two's complement number to a long integer
+long twosComplement(unsigned long num, unsigned char N) {
+    long output;
+    if (num >> (N - 1)) {
+        output = -1 * (~((num - 1) << (33 - N)) >> (33 - N));
+        if (output == 0) {
+            output = -1;
+            for (int i = 1; i <= N - 1; i++) {
+                output = output * 2;
+            }
+        }
+    } else {
+        output = num;
+    }
     return output;
 }
